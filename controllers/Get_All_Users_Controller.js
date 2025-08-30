9b345aa2085a440dd0c7da861e6c7b1a9535c182
Rasheed963 <rasheedhaddad2018@gmail.com>
2025-08-30 16:25:46 +0300

    AWAAAAAAAIIIIITTTTT

--- controllers/Get_All_Users_Controller.js
+++ controllers/Get_All_Users_Controller.js
@@ -2,7 +2,7 @@ import User from "../models/User.js";
 
 export const Get_All_Users = async (req, res) => {
   try {
-    const Users = User.find();
+    const Users = await User.find();
     if ((await Users).length === 0) {
       res.status(404).json({ message: "لا يوجد مستخدمين نهائيا" });
     }
